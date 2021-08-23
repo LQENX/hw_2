91bf15702362004a72a6837f62571dc9753c2ae2
Daniil Gerasimov <Dan>
2021-08-24 00:43:05 +0400

    Customizing the app that the template produces for lesson 1.4

---
+++ app/lesson_1_4/src/main/java/com/mycompany/lesson_1_4/BasicActivity.kt
@@ -0,0 +1,15 @@
+package com.mycompany.lesson_1_4
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class BasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
