0ebc70b388b230696aaf90210afe072e53be385d
Daniil Gerasimov <Dan>
2021-08-24 01:32:10 +0400

    Message between activities with intent. Lesson 2.1

--- app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/MainActivity.kt
+++ app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/MainActivity.kt
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String? = MainActivity::class.simpleName
 
+    private lateinit var mMessageEditText: EditText
+
+    companion object {
+        const val EXTRA_MESSAGE = "com.mycompany.lesson_2_1.MESSAGE"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mMessageEditText = findViewById(R.id.editText_main)
     }
 
     fun launchSecondActivity(view: View) {
         Log.d(TAG, "Button clicked!")
         val intent = Intent(this, SecondActivity().javaClass)
+        val message = mMessageEditText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
         startActivity(intent)
+
     }
 }
--- app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/SecondActivity.kt
+++ app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/SecondActivity.kt
@@ -2,10 +2,14 @@ package com.mycompany.lesson_2_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.text_message)
+        textView.setText(message)
     }
 }
