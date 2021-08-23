957292f62f464820045a1870b4b9d014393b109c
Daniil Gerasimov <Dan>
2021-08-24 01:52:35 +0400

    lesson 2.1 is done

--- app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/MainActivity.kt
+++ app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/MainActivity.kt
@@ -6,20 +6,27 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String? = MainActivity::class.simpleName
 
     private lateinit var mMessageEditText: EditText
+    private lateinit var mReplyHeadTextView: TextView
+    private lateinit var mReplyTextView: TextView
 
     companion object {
         const val EXTRA_MESSAGE = "com.mycompany.lesson_2_1.MESSAGE"
+        const val TEXT_REQUEST = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mMessageEditText = findViewById(R.id.editText_main)
+        mReplyHeadTextView = findViewById(R.id.text_header_reply)
+        mReplyTextView = findViewById(R.id.text_message_reply)
+
     }
 
     fun launchSecondActivity(view: View) {
@@ -27,7 +34,18 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SecondActivity().javaClass)
         val message = mMessageEditText.text.toString()
         intent.putExtra(EXTRA_MESSAGE, message)
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
+                mReplyHeadTextView.visibility = View.VISIBLE
+                mReplyTextView.setText(reply)
+                mReplyTextView.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/SecondActivity.kt
+++ app/lesson_2_1/src/main/java/com/mycompany/lesson_2_1/SecondActivity.kt
@@ -1,15 +1,34 @@
 package com.mycompany.lesson_2_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var mReply: EditText
+
+
+    companion object {
+        const val EXTRA_REPLY = "com.mycompany.lesson_2_1.REPLY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
         val textView = findViewById<TextView>(R.id.text_message)
         textView.setText(message)
+        mReply = findViewById(R.id.editText_second)
+    }
+
+    fun returnReply(view: View) {
+        val reply = mReply.text.toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
     }
 }
